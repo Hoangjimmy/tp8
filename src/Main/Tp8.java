49078dae48f6815193a8ab4b463d260ba8bd945d
hoangjimmy <yumiquet@gmail.com>
2015-11-17 09:51:11 +0100

    C converter

--- src/Main/Tp8.java
+++ src/Main/Tp8.java
@@ -5,6 +5,13 @@
  */
 package Main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author hoangjim
@@ -15,7 +22,40 @@ public class Tp8 {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+
+		if (args.length < 1)
+			System.out.println("Usage tp8 <JavaClassName> <GeneratedC++ClassName>");
+		else
+			hppMaker(args[0]);
+
 	}
-	
+
+	public static void hppMaker(String className) {
+
+		File f = new File(className + ".hpp");
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			Class c = Class.forName(className);
+			try {
+				FileWriter writer = new FileWriter(f, true);
+				sb = sb.append("class ").append(className).append(" { \n").append("private : \n");
+				Method[] meth = c.getMethods();
+				for (int i = 0; i < 6; ++i)
+					sb.append(meth[i].toString().replace("public","")).append(";\n");
+				sb.append("}");
+				writer.write(sb.toString());
+				writer.flush();
+				writer.close();
+
+			} catch (IOException ex2) {
+				Logger.getLogger(Tp8.class.getName()).log(Level.SEVERE, null, ex2);
+			}
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+
+		}
+
+	}
+
 }
---
+++ src/TestClass.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hoangjim
+ */
+public class TestClass {
+	
+	private int a;
+	private long b;
+	private String c;
+
+	/**
+	 * @return the a
+	 */
+	public int getA() {
+		return a;
+	}
+
+	/**
+	 * @param a the a to set
+	 */
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	/**
+	 * @return the b
+	 */
+	public long getB() {
+		return b;
+	}
+
+	/**
+	 * @param b the b to set
+	 */
+	public void setB(long b) {
+		this.b = b;
+	}
+
+	/**
+	 * @return the c
+	 */
+	public String getC() {
+		return c;
+	}
+
+	/**
+	 * @param c the c to set
+	 */
+	public void setC(String c) {
+		this.c = c;
+	}
+	
+	
+	
+	
+}
